b81e864ecfdcb9738cb21736e006ec7f56b9e3fd
Tae young Kim <tyk0707@gmail.com>
2025-06-26 19:54:31 +0900

    자바 파일 생성

---
+++ java-start/src/variable/Var1.java
@@ -0,0 +1,10 @@
+package variable;
+
+// 변수 //
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
