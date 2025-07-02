b30222a9d00dc1ef563f9d19dcf058309d248a03
Tae young Kim <tyk0707@gmail.com>
2025-07-03 03:03:19 +0900

    제네릭 코드 추가

---
+++ java-mid2/src/generic/ex01/IntegerBox.java
@@ -0,0 +1,13 @@
+package generic.ex01;
+
+public class IntegerBox {
+    private Integer value;
+
+    public void set(Integer value) {
+        this.value = value;
+    }
+
+    public Integer get() {
+        return this.value;
+    }
+}
