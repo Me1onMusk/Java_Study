6e13c4a82c061aabd82c0005fb665088c2ca1c31
Tae young Kim <tyk0707@gmail.com>
2025-07-22 01:35:09 +0900

    자바코드 추가

---
+++ java-adv2/src/was/v3/HttpServerV3.java
@@ -0,0 +1,30 @@
+package was.v3;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static util.MyLogger.log;
+
+// 서버 //
+public class HttpServerV3 {
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(10);  //10개 스레드 고정
+    private final int port;
+
+    public HttpServerV3(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        ServerSocket  serverSocket = new ServerSocket(port);
+        log("서버 시작 port: " + port);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            executor.submit(new HttpRequestHandlerV3(socket));
+        }
+    }
+}
