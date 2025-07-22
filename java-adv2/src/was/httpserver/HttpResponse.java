89dc342af917700c9ed2b147cb3f5c01caf87d2a
Tae young Kim <tyk0707@gmail.com>
2025-07-22 10:11:21 +0900

    코드 추가

--- java-adv2/src/was/httpserver/HttpResponse.java
+++ java-adv2/src/was/httpserver/HttpResponse.java
@@ -1,4 +1,36 @@
 package was.httpserver;
 
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+import static java.nio.charset.StandardCharsets.*;
+
+// 서버 -> 클라이언트
 public class HttpResponse {
+
+    private final PrintWriter writer;
+    private int statusCode = 200;
+    private final StringBuilder bodyBuilder = new StringBuilder();
+    private String contentType = "text/html; charset=utf-8";
+
+    public HttpResponse(PrintWriter writer) {
+        this.writer = writer;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public void writeBody(String body) {
+        bodyBuilder.append(body);
+    }
+
+    public void flush() {
+        int contentLength = bodyBuilder.toString().getBytes(UTF_8).length;
+        writer.println("HTTP/1.1 " + statusCode + " "+"OK");
+    }
 }
