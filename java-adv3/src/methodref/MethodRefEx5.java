3c5618112b2a521e9c0b256ecb52de02b68c7706
Tae young Kim <tyk0707@gmail.com>
2025-07-25 12:05:02 +0900

    자바 코드 추가

---
+++ java-adv3/src/methodref/MethodRefEx5.java
@@ -0,0 +1,15 @@
+package methodref;
+
+import java.util.List;
+
+public class MethodRefEx5 {
+    public static void main(String[] args) {
+
+        List<Person> personList = List.of(
+                new Person("Kim"),
+                new Person("Park"),
+                new Person("Lee")
+        );
+
+    }
+}
