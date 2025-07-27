7e1ec476570f9a87af81b62093a6ef61f7662850
Tae young Kim <tyk0707@gmail.com>
2025-07-27 15:50:57 +0900

    자바 코드 추가

--- java-mid2/src/collection/link/Node.java
+++ java-mid2/src/collection/link/Node.java
@@ -9,4 +9,19 @@ public class Node {
         this.item = item;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null)
+                sb.append("->");
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
 }
