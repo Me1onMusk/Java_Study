80fc7929fbf98db40f651633909f8f0c3c29414f
Tae young Kim <tyk0707@gmail.com>
2025-07-31 09:30:28 +0900

    자바 코드 추가

---
+++ java-all/src/adv1/thread/control/ThreadInfoMain.java
@@ -0,0 +1,13 @@
+package adv1.thread.control;
+
+public class ThreadInfoMain {
+    public static void main(String[] args) {
+
+        // main 스레드
+        Thread mainThread = Thread.currentThread();
+
+        // myThread 스레드
+
+
+    }
+}
