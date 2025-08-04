e372233404fd5407640d8fdd4feec35d7dfa8c6b
Tae young Kim <tyk0707@gmail.com>
2025-08-04 22:42:39 +0900

    자바 코드 추가

---
+++ java-adv3/src/defaultmethod/ex1/Notifier.java
@@ -0,0 +1,8 @@
+package defaultmethod.ex1;
+
+import java.time.LocalDateTime;
+
+public interface Notifier {
+    // 알림을 보내는 기본 기능
+    void notify(String message);
+}
--- java-adv3/src/optional/OptionalProcessingMain.java
+++ java-adv3/src/optional/OptionalProcessingMain.java
@@ -10,52 +10,52 @@ public class OptionalProcessingMain {
 
         // 값이 존재하면 Consumer 실행, 없으면 아무 일도 하지 않음
         System.out.println("=== 1. ifPresent() ===");
-        optValue.ifPresent(v -> System.out.println("optValue 값: " + v));
-        optEmpty.ifPresent(v -> System.out.println("optEmpty 값: " + v)); // 실행 X
+        optValue.ifPresent(v -> System.out.println("optValue 값: " + v));  //optValue 값: Hello
+        optEmpty.ifPresent(v -> System.out.println("optEmpty 값: " + v)); //실행 X
 
         // 값이 있으면 Consumer 실행, 없으면 Runnable 실행
         System.out.println("=== 2. ifPresentOrElse() ===");
         optValue.ifPresentOrElse(
-                v -> System.out.println("optValue 값: " + v),
-                () -> System.out.println("optValue는 비어있음")
+                v -> System.out.println("optValue 값: " + v),  // 값이 있으면 -> optValue 값: Hello
+                () -> System.out.println("optValue는 비어있음")       // 값이 없으면
         );
         optEmpty.ifPresentOrElse(
-                v -> System.out.println("optEmpty 값: " + v),
-                () -> System.out.println("optEmpty는 비어있음")
+                v -> System.out.println("optEmpty 값: " + v),  // 값이 있으면
+                () -> System.out.println("optEmpty는 비어있음")       // 값이 없으면 -> optEmpty는 비어있음
         );
 
         // 값이 있으면 Function 적용 후 Optional로 반환, 없으면 Optional.empty()
         System.out.println("=== 3. map() ===");
         Optional<Integer> lengthOpt1 = optValue.map(String::length);
-        System.out.println("optValue.map(String::length) = " + lengthOpt1);
+        System.out.println("optValue.map(String::length) = " + lengthOpt1);  // optValue.map(String::length) = Optional[5]
         Optional<Integer> lengthOpt2 = optEmpty.map(String::length);
-        System.out.println("optEmpty.map(String::length) = " + lengthOpt2); // map()과 유사하나, 이미 Optional을 반환하는 경우 중첩을 제거
-        System.out.println("=== 4. flatMap() ===");
-        System.out.println("[map]");
+        System.out.println("optEmpty.map(String::length) = " + lengthOpt2); // optEmpty.map(String::length) = Optional.empty
 
         // Optional[Hello] -> Optional[Optional[HELLO]]
+        System.out.println("=== 4. flatMap() ===");
+        System.out.println("[map]");
         Optional<Optional<String>> nestedOpt = optValue.map(s -> Optional.of(s));
-        System.out.println("optValue = " + optValue);
-        System.out.println("nestedOpt = " + nestedOpt);
-        System.out.println("[flatMap]");
+        System.out.println("optValue = " + optValue);    // optValue = Optional[Hello]
+        System.out.println("nestedOpt = " + nestedOpt);  // nestedOpt = Optional[Optional[Hello]]
 
         // flatMap을 사용하면 한 번에 평탄화
         // Optional[Hello] -> Optional[HELLO]
+        System.out.println("[flatMap]");
         Optional<String> flattenedOpt = optValue.flatMap(s -> Optional.of(s));
-        System.out.println("optValue = " + optValue);
-        System.out.println("flattenedOpt = " + flattenedOpt);
-        System.out.println("=== 5. filter() ===");
+        System.out.println("optValue = " + optValue);          // optValue = Optional[Hello]
+        System.out.println("flattenedOpt = " + flattenedOpt);  //flattenedOpt = Optional[Hello]
 
         // 값이 있고 조건을 만족하면 그 값을 그대로, 불만족시 Optional.emtpy()
+        System.out.println("=== 5. filter() ===");
         Optional<String> filtered1 = optValue.filter(s -> s.startsWith("H"));
         Optional<String> filtered2 = optValue.filter(s -> s.startsWith("X"));
-        System.out.println("filter(H) = " + filtered1); // Optional[Hello]
-        System.out.println("filter(X) = " + filtered2); // Optional.empty
-        System.out.println("=== 6. stream() ===");
+        System.out.println("filter(H) = " + filtered1); // filter(H) = Optional[Hello]
+        System.out.println("filter(X) = " + filtered2); // filter(X) = Optional.empty
 
         // 값이 있으면 단일 요소 스트림, 없으면 빈 스트림
+        System.out.println("=== 6. stream() ===");
         optValue.stream()
-                .forEach(s -> System.out.println("optValue.stream() -> " + s));
+                .forEach(s -> System.out.println("optValue.stream() -> " + s));  // optValue.stream() -> Hello
 
         // 값이 없으므로 실행 안 됨
         optEmpty.stream()
