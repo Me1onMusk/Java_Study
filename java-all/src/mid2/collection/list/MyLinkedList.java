0839dc21108cab1b7264d211898147300128d00f
Tae young Kim <tyk0707@gmail.com>
2025-08-21 17:44:28 +0900

    자바 코드 추가

--- java-all/src/mid2/collection/list/MyLinkedList.java
+++ java-all/src/mid2/collection/list/MyLinkedList.java
@@ -1,37 +1,114 @@
 package mid2.collection.list;
 
+import mid2.collection.link.Node;
+
 public class MyLinkedList <E> implements MyList<E>{
 
+    private Node<E> first;
     private int size = 0;
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
-    public void add(E e) {}
+    public void add(E e) {
+        Node<E> newNode = new Node<>(e);
+        if(first == null)
+            first = newNode;
+        else {
+            Node<E> lastNode = getLastNode();
+            lastNode.next = newNode;
+        }
+        size++;
+    }
+
+    private Node<E> getLastNode() {
+        Node<E> x = first;
+        while (x.next != null) {
+            x = x.next;
+        }
+        return x;
+    }
 
     @Override
-    public void add(int index, E e) {}
+    public void add(int index, E e) {
+        Node<E> newNode = new Node<>(e);
+        if(index == 0) {
+            newNode.next = first;
+            first = newNode;
+        } else {
+            Node<E> prev = getNode(index - 1);
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+        size++;
+    }
+
+    private Node<E> getNode(int index) {
+        Node<E> x = first;
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
 
     @Override
     public E get(int index) {
-        return null;
+        Node<E> node = getNode(index);
+        return node.item;
     }
 
     @Override
-    public E set(int index, E e) {
-        return null;
+    public E set(int index, E element) {
+        Node<E> x = getNode(index);
+        E oldValue = x.item;
+        x.item = element;
+        return oldValue;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        Node<E> removeNode = getNode(index);
+        E removedItem = removeNode.item;
+        if(index == 0)
+            first = removeNode.next;
+        else {
+            Node<E> prev = getNode(index - 1);
+            prev.next = removeNode.next;
+        }
+        removeNode.item = null;
+        removeNode.next = null;
+        size--;
+        return removedItem;
     }
 
     @Override
-    public int indexOf(E e) {
-        return 0;
+    public int indexOf(E o) {
+        int index = 0;
+        for(Node<E> x = first; x != null; x = x.next) {
+            if(o.equals(x.item))
+                return index;
+            index++;
+        }
+        return -1;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node<E> temp = this;
+        sb.append("[");
+        while (temp != null) {
+            sb.append(temp.item);
+            if (temp.next != null)
+                sb.append("->");
+            temp = temp.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+
 }
