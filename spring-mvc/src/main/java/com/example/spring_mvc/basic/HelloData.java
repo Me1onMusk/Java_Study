0b5c139e6bb4d65664691a58f88439c93fc05c9c
Tae young Kim <tyk0707@gmail.com>
2025-09-05 07:04:37 +0900

    자바 코드

---
+++ spring-mvc/src/main/java/com/example/spring_mvc/basic/HelloData.java
@@ -0,0 +1,12 @@
+package com.example.spring_mvc.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String userName;
+    private int age;
+
+}
